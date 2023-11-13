99bd7392cb50ba5bf78db1b6ef9b21f4bc0d9b85
sewellad <144348593+sewellad@users.noreply.github.com>
2023-11-13 08:50:04 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    game.reset()
+})
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+let number = randint(1, 10)
+let guess = game.askForNumber("1 to 10")
+if (guess == number) {
+    mySprite.sayText("Great Job!")
+} else if (false) {
+    mySprite.sayText("Sorry! That's not my number.")
+} else if (number < guess) {
+    mySprite.sayText("Sorry! That's too high.")
+} else if (number > guess) {
+    mySprite.sayText("Sorry! That's too low.")
+}
